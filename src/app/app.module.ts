944ef04856997b2fde4643970155343a6e126bf3
Arthur Monney <monneylobe@gmail.com>
2022-09-02 23:31:37 +0100

    :construction: wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './core/routes/app-routing.module';
+import { AppComponent } from './core/components/app/app.component';
+import { ThemeModule } from './shared/themes/theme.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    StoreModule.forRoot({}, {}),
+    ThemeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
