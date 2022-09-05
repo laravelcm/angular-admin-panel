b24445b35cc7a6aab3d74133260784736d483b60
Arthur Monney <monneylobe@gmail.com>
2022-09-05 18:06:21 +0100

    :heavy_plus_sign: ajout de ngx-env pour la gestion des environnements de dev & prod

---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+declare var process: {
+  env: {
+    NG_APP_NAME: string;
+    NG_APP_DEBUG: false;
+    NG_APP_VERSION: string;
+    NG_APP_BASE_URL: string;
+    NG_APP_API_URL: string;
+    NG_APP_API_VERSION: string;
+    NG_APP_SENTRY_DSN: string | null;
+    NG_APP_SENTRY_TRACES_SAMPLE_RATE: string;
+  };
+};
