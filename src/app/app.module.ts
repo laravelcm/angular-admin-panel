6bd8665ea4209516dc58bee08371f80ad62ae06b
Arthur Monney <monneylobe@gmail.com>
2022-09-05 12:16:14 +0100

    :construction: wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './core/routes/app-routing.module';
 import { AppComponent } from './core/components/app/app.component';
 
@@ -18,6 +20,7 @@ import { AppComponent } from './core/components/app/app.component';
     FormsModule,
     ReactiveFormsModule,
     StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/modules/index.ts
@@ -0,0 +1,7 @@
+import { AuthenticationModule } from './authentication/authentication.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+export const MODULES = [
+  AuthenticationModule,
+  DashboardModule,
+];
