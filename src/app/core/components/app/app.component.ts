5b7b4968795b40a13a0d143006c6a0c5da5844d4
Arthur Monney <monneylobe@gmail.com>
2022-09-05 16:59:23 +0100

    :recycle: refactoring de l'app component

--- src/app/core/components/app/app.component.ts
+++ src/app/core/components/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
   title = 'Cosna Cpanel';
