6ad7f193931452c130f08f7896d436ba6795d977
Arthur Monney <monneylobe@gmail.com>
2022-09-24 23:11:25 +0100

    :recycle: refcatoring global de l'app

--- src/app/core/components/app/app.component.ts
+++ src/app/core/components/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cosna-root',
+  selector: 'admin-root',
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
-  title = 'Cosna Cpanel';
+  title = 'Admin Cpanel';
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,31 +11,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          50: '#ECFFEC',
-          100: '#E3FFE3',
-          200: '#CEEACD',
-          300: '#b0ddae',
-          400: '#74c372',
-          500: '#39a935',
-          600: '#339830',
-          700: '#2b7f28',
-          800: '#226520',
-          900: '#1c531a',
-        },
+        primary: colors.green,
         secondary: {
           lighter: '#F29121',
           light: '#F8B133',
-          50: '#FEF9F3',
-          100: '#FDF4E7',
-          200: '#FAE2C4',
-          300: '#F7D1A1',
-          400: '#F2AF5A',
-          500: '#EC8C13',
-          600: '#D47E11',
-          700: '#B1690E',
-          800: '#8E540B',
-          900: '#744509',
+          ...colors.emerald
         }
       },
       fontFamily: {
