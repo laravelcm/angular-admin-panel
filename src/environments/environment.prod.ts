3b5af221663a1ea7c73d8dd1e54560cd7fe5aebe
Arthur Monney <monneylobe@gmail.com>
2022-10-13 18:58:20 +0100

    :globe_with_meridians: Enable localization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  appName: process.env.NG_APP_NAME,
+  debug: process.env.NG_APP_DEBUG,
+  version: process.env.NG_APP_VERSION,
+  baseUrl: process.env.NG_APP_BASE_URL,
+  apiUrl: process.env.NG_APP_API_URL,
+  apiVersion: process.env.NG_APP_API_VERSION,
+  sentryDsn: process.env.NG_APP_SENTRY_DSN,
+  sentryTracesSampleRate: process.env.NG_APP_SENTRY_TRACES_SAMPLE_RATE,
 };
