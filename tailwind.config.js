aa10e3ef644eb7fa755a77240769bb7e1e762018
Arthur Monney <monneylobe@gmail.com>
2023-07-03 19:29:48 +0200

    Add dependabot and upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
   ],
 };
